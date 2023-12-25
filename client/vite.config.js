19b3a7de6e2ee69d18d8da9da8e0b4c2bc2cba4a
Murat Mutlu <muratmutlu010@gmail.com>
2023-12-25 22:30:33 +0300

    Update proxy target URL in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000' || 'https://bench-yf02.onrender.com',
+        target: 'https://bench-yf02.onrender.com',
         secure: true,
       },
     },
